455a929a78753ed47be12619fb474ae7cc0f5549
Gibson Thomas <gibsonthomass@gmail.com>
2018-01-13 21:43:23 +0530

    Added BL project

---
+++ MVCATM/MVCATM/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLogic.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCATM.Controllers
+{
+    public class LoginController : Controller
+    {
+        IUserAccountRepository _repository;
+
+        public LoginController(IUserAccountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
